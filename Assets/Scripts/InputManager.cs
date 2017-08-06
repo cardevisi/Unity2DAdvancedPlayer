b7bd6b7a6b04f1eb117391a19086104a610969f0
Carlos Augusto <cardevisi>
2017-08-06 12:31:46 -0300

    [MASTER] first upload

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Buttons
+{
+    Right,
+    Left
+}
+
+public enum Condition
+{
+    GreatherThan,
+    LessThan
+}
+
+[System.Serializable]
+public class InputAxisState
+{
+    public string axisName;
+    public float offValue;
+    public Buttons button;
+    public Condition condition;
+
+    public bool value
+    {
+        get {
+            var val = Input.GetAxis(axisName);
+
+            switch(condition)
+            {
+                case Condition.GreatherThan:
+                    return val > offValue;
+                break;
+                case Condition.LessThan:
+                    return val < offValue;
+                break;
+            }
+
+            return false;
+        }
+    }
+}
+
+
+public class InputManager : MonoBehaviour {
+       
+    public InputAxisState[] inputs;
+    public InputState inputState;
+
+    void Start () {
+		
+	}
+    
+    void Update () {
+        foreach (var input in inputs)
+        {
+            inputState.SetButtonState(input.button, input.value);
+        }
+	}
+}
---
+++ Assets/Scripts/InputState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonState {
+    public bool value;
+    public float holdTime = 0;
+}
+
+public class InputState : MonoBehaviour {
+
+    private Dictionary<Buttons, ButtonState> buttonsStates = new Dictionary<Buttons, ButtonState>();
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+
+    }
+
+    public void SetButtonState(Buttons key, bool value)
+    {
+        if(!buttonsStates.ContainsKey(key))
+        {
+            buttonsStates.Add(key, new ButtonState());
+        }
+
+        var state = buttonsStates[key];
+         if(state.value && !value )
+        {
+            Debug.Log("Button " + key + " Released " + state.holdTime);
+            state.holdTime = 0;
+        } else if (state.value && value)
+        {
+            state.holdTime += Time.deltaTime;
+            Debug.Log("Button " + key + " down " + state.holdTime);
+        }
+
+        state.value = value;
+    }
+
+    public bool GetButtonValue(Buttons key)
+    {
+        if (buttonsStates.ContainsKey(key))
+        {
+            return buttonsStates[key].value;
+        } else
+        {
+            return false;
+        }
+    }
+}
